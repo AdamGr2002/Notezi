8327cbebb0caf7959049e0c668221e8362f92735
adam <adam.guerchi@tek-up.de>
2025-08-29 17:08:00 +0100

    feat: add Tailwind CSS and related dependencies, for shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  alias: {
+    '@components': './components',  // Example: alias for components folder
+    '@utils': './utils'             // Example: alias for utils folder
+  },
   modules: [
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/eslint',
-    '@nuxt/fonts'
-  ]
+    '@nuxt/fonts',
+    'shadcn-nuxt',
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
